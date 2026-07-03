83d1099368504fadb7a21d03e83c1aa7b7dd1561
yzx_2 <2931341372@qq.com>
2026-07-04 01:20:42 +0800

    修改第一次

--- src/main/java/com/yzx/Main.java
+++ src/main/java/com/yzx/Main.java
@@ -7,6 +7,6 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("第一次提交");
-
+        System.out.printf("第二次提交");
     }
 }
